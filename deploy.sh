3b7ac865ae0dcb7a75e6ff99de65739c0401f4e1
Mukund Sharma <mukund_bihari@mckinsey.com>
2018-09-09 07:37:48 -0400

    removed old scripts

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$(sudo docker top game2048)" ]
-then
-        echo "Container for Application game2048 already exists."
-else
-        sudo docker run -i -d -p 32048:80 --name=game2048 biharimukund/game2048
-fi
-

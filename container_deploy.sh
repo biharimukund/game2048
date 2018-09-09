611324e1190464c84048a89a23ca937c5f9cac97
dguyhasnoname <biharimukund@gmail.com>
2018-09-09 17:19:48 +0530

    Update container_deploy.sh

--- container_deploy.sh
+++ container_deploy.sh
@@ -4,6 +4,7 @@ if [ "$(sudo docker top game2048)" ]
 then
         echo "Container for Application game2048 already exists."
 else
+        echo "Deploying new container for gaem2048"
         sudo docker run -i -d -p 32048:80 --name=game2048 biharimukund/game2048
 fi
 

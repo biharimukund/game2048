e310da2e3fa92838ee6c5ee2e53ca1b48307c001
dguyhasnoname <biharimukund@gmail.com>
2018-09-09 18:39:36 +0530

    updated port

--- health_check.sh
+++ health_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CONTAINER_IP=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' game2048)
-if [ "$(curl -s -o /dev/null -I -w "%{http_code}" http://$CONTAINER_IP:32048)" == "200" ]
+if [ "$(curl -s -o /dev/null -I -w "%{http_code}" http://$CONTAINER_IP:80)" == "200" ]
 then
 	echo "Application is UP and accessible!"
 else

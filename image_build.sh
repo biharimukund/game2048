10c68aeced4c12fd7bcb42be7ebd073e4b6d93d3
dguyhasnoname <biharimukund@gmail.com>
2018-10-02 02:31:21 +0530

    Update image_build.sh

--- image_build.sh
+++ image_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -s "$(sudo docker images biharimukund/game2048 -q)" ]
+if [ -s "$(docker images biharimukund/game2048 -q)" ]
 then
-	sudo docker build -t biharimukund/game2048 .
+	docker build -t biharimukund/game2048 .
 else
 	echo "The image already exists."
 fi
